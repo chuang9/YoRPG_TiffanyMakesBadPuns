509af1eeef66f6aa36769f2e66c87057a646b484
tmoi29 <tmoi@stuy.edu>
2016-11-23 19:49:01 -0500

    Updating Character

---
+++ Character.java
@@ -0,0 +1,34 @@
+// Team Tiffany Makes Bad Puns -- Bayan Berri, Chester Huang, Tiffany Moi
+// APCS1 pd5
+// HW34 -- Ye Olde Role Playing Game, Unchained
+// 2016-11-23
+
+public abstract class Character{
+    protected String name;
+    protected int health;
+    protected int strength;
+    protected int defense;
+    protected double atkRating;
+
+    public boolean isAlive(){
+        return health > 0;
+    }
+    public String getName(){
+	return name;
+    }
+    public int getDefense(){
+        return defense;
+    }
+    public void lowerHP(int damage){
+        health -= damage;
+    }
+    public int attack(Character c){
+        int d = (int)((strength * atkRating) - c.defense);
+        c.lowerHP(d);
+        return d;
+    }
+    public abstract void normalize();
+    public abstract void specialize();
+    public abstract String about();
+    public abstract int specialAtk(String s);
+}
